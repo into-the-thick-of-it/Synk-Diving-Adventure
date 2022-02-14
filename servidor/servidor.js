7951cc3c79cf8a7c9a4f21784c785208cf655c1e
Py3tra <pyetra.g@aluno.ifsc.edu.br>
2022-02-14 20:37:37 +0000

    semana 5 e 6

---
+++ servidor/servidor.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const PORT = process.env.PORT || 3000;
+var jogadores = {
+    primeiro: undefined,
+    segundo: undefined,
+};
+  
+// Disparar evento quando jogador entrar na partid
+io.on("connection", function (socket) {
+  if (jogadores.primeiro === undefined) {
+     jogadores.primeiro = socket.id;
+    } else if (jogadores.segundo === undefined) {
+      jogadores.segundo = socket.id;
+    }
+    io.emit("jogadores", jogadores);
+    console.log("+Lista de jogadores: %s", jogadores);
+  
+    // Sinalização de áudio: oferta
+    socket.on("offer", (socketId, description) => {
+      socket.to(socketId).emit("offer", socket.id, description);
+    });
+  
+    // Sinalização de áudio: atendimento da oferta
+    socket.on("answer", (socketId, description) => {
+      socket.to(socketId).emit("answer", description);
+    });
+  
+    // Sinalização de áudio: envio dos candidatos de caminho
+    socket.on("candidate", (socketId, signal) => {
+      socket.to(socketId).emit("candidate", signal);
+    });
+  
+    // Disparar evento quando jogador sair da partida
+    socket.on("disconnect", function () {
+      if (jogadores.primeiro === socket.id) {
+        jogadores.primeiro = undefined;
+      }
+      if (jogadores.segundo === socket.id) {
+        jogadores.segundo = undefined;
+      }
+      io.emit("jogadores", jogadores);
+      console.log("-Lista de jogadores: %s", jogadores);
+    });
+});
+//
+app.use("/twine/", express.static("../cliente-twine"));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
